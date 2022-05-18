a63f3b511320bd08c19931ad0020716103c254e0
Ahmed Khalaf <pxlshpr@gmail.com>
2022-05-18 13:49:28 +0500

    tag increment

--- Sources/VisionSugar/Box.swift
+++ Sources/VisionSugar/Box.swift
@@ -1,9 +1,15 @@
 import Vision
 
 public struct Box: Identifiable, Hashable {
-    public var id = UUID()
+    public var id: UUID
     public var observation: VNRecognizedTextObservation
     public var rect: CGRect
+    
+    public init(observation: VNRecognizedTextObservation, rect: CGRect) {
+        self.id = observation.uuid
+        self.observation = observation
+        self.rect = rect
+    }
     
     public func hash(into hasher: inout Hasher) {
         hasher.combine(id)
