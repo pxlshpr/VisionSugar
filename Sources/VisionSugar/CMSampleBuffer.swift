9b5952670833e225214a87ba4de361de9d9f588c
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-14 15:05:55 +0500

    tag increment

--- Sources/VisionSugar/CMSampleBuffer.swift
+++ Sources/VisionSugar/CMSampleBuffer.swift
@@ -44,20 +44,18 @@ extension CMSampleBuffer {
 
 extension CMSampleBuffer {
     
-    public func recognizedTextSet(for config: RecognizeTextConfiguration, inContentSize contentSize: CGSize) async throws -> RecognizedTextSet {
-        var textSets: [RecognizedTextSet] = []
-        try recognizeTextObservations(configs: [config]) { observationSet in
-            guard let observationSet else {
-                return
-            }
-            let textSet = RecognizedTextSet(
-                config: observationSet.config,
-                texts: self.recognizedTexts(from: observationSet.observations, inContentSize: contentSize)
-            )
-            textSets.append(textSet)
-        }
-        return textSets.first ?? RecognizedTextSet(config: config, texts: [])
+    public func recognizedTextSet(for config: RecognizeTextConfiguration) async throws -> RecognizedTextSet {
+        try await config.recognizedTextSet(recognizeTextObservationsHandler: recognizeTextObservations)
     }
+
+//    public func recognizedTextSet(for config: RecognizeTextConfiguration) async throws -> RecognizedTextSet {
+//        var textSets: [RecognizedTextSet] = []
+//        try recognizeTextObservations(config: config) { observationSet in
+//            guard let observationSet else { return }
+//            textSets.append(observationSet.textSet)
+//        }
+//        return textSets.first ?? RecognizedTextSet(config: config, texts: [])
+//    }
     
 //    public func recognizeTextObservationsAndBarcodes(
 //        configs: [RecognizeTextConfiguration],
@@ -69,54 +67,45 @@ extension CMSampleBuffer {
 //    }
     
     public func recognizeTextObservations(
-        configs: [RecognizeTextConfiguration],
-        completion: @escaping ((RecognizedTextObservationSet?) -> Void)
+        config: RecognizeTextConfiguration,
+        completion: @escaping TextObservationSetHandler
     ) throws {
-        let requests = recognizeTextRequests(for: configs, completion: completion)
+        let request = config.recognizeTextRequest(completion: completion)
         let requestHandler = VNImageRequestHandler(cmSampleBuffer: self, orientation: .right)
-        try requestHandler.perform(requests)
-    }
-    
-    func recognizeTextRequests(
-        for configs: [RecognizeTextConfiguration],
-        completion: @escaping ((RecognizedTextObservationSet) -> ())
-    ) -> [VNRecognizeTextRequest] {
-        var requests: [VNRecognizeTextRequest] = []
-        for config in configs {
-            let request = VNRecognizeTextRequest { request, error in
-                guard let observations = request.results as? [VNRecognizedTextObservation] else {
-                    return
-                }
-                
-                let observationSet = RecognizedTextObservationSet(config: config, observations: observations)
-                completion(observationSet)
-            }
-            
-            if let languages = config.languages {
-                request.recognitionLanguages = languages
-            }
-            request.recognitionLevel = config.level
-            request.usesLanguageCorrection = config.languageCorrection
-            request.customWords = config.customWords
-            requests.append(request)
-        }
-        return requests
-    }
-    
-    public func recognizedTexts(from observations: [VNRecognizedTextObservation], inContentSize contentSize: CGSize) -> [RecognizedText] {
-        observations.map { observation in
-            recognizedText(from: observation, inContentSize: contentSize)
-        }
-    }
-    
-    public func recognizedText(from observation: VNRecognizedTextObservation, inContentSize contentSize: CGSize) -> RecognizedText {
-        
-        return RecognizedText(observation: observation,
-                              rect: observation.boundingBox,
-                              boundingBox: observation.boundingBox
-        )
-//        let rect = observation.boundingBox.rectForSize(UIScreen.main.bounds.size)
-//        return RecognizedText(observation: observation, rect: rect, boundingBox: observation.boundingBox)
+        try requestHandler.perform([request])
     }
 }
 
+extension RecognizeTextConfiguration {
+    
+    public func recognizedTextSet(
+        recognizeTextObservationsHandler: @escaping TextObservationsHandler
+    ) async throws -> RecognizedTextSet {
+        var textSets: [RecognizedTextSet] = []
+        try recognizeTextObservationsHandler(self) { observationSet in
+            guard let observationSet else { return }
+            textSets.append(observationSet.textSet)
+        }
+        return textSets.first ?? RecognizedTextSet(config: self, texts: [])
+    }
+    
+    func recognizeTextRequest(completion: @escaping TextObservationSetHandler) -> VNRecognizeTextRequest
+    {
+        let request = VNRecognizeTextRequest { request, error in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                return
+            }
+            
+            let observationSet = RecognizedTextObservationSet(config: self, observations: observations)
+            completion(observationSet)
+        }
+        
+        if let languages = self.languages {
+            request.recognitionLanguages = languages
+        }
+        request.recognitionLevel = self.level
+        request.usesLanguageCorrection = self.languageCorrection
+        request.customWords = self.customWords
+        return request
+    }
+}
--- Sources/VisionSugar/UIImage.swift
+++ Sources/VisionSugar/UIImage.swift
@@ -2,109 +2,37 @@ import UIKit
 import Vision
 import SwiftUISugar
 
-public struct RecognizeTextConfiguration {
-    
-    public let level: VNRequestTextRecognitionLevel
-    public let languageCorrection: Bool
-    public let languages: [String]?
-    public let customWords: [String]
-    
-    public init(
-        level: VNRequestTextRecognitionLevel = .accurate,
-        languageCorrection: Bool = true,
-        languages: [String]? = nil,
-        customWords: [String] = []
-    ) {
-        self.level = level
-        self.languageCorrection = languageCorrection
-        self.languages = languages
-        self.customWords = customWords
-    }
-    
-    public static let accurate = RecognizeTextConfiguration(level: .accurate, languageCorrection: true)
-    public static let accurateWithoutLanguageCorrection = RecognizeTextConfiguration(level: .accurate, languageCorrection: false)
-    public static let fast = RecognizeTextConfiguration(level: .fast)
-}
-
-public struct RecognizedTextSet {
-    public let config: RecognizeTextConfiguration
-    public let texts: [RecognizedText]
-}
-
-public struct RecognizedTextObservationSet {
-    public let config: RecognizeTextConfiguration
-    public let observations: [VNRecognizedTextObservation]
-}
+public typealias TextObservationSetHandler = ((RecognizedTextObservationSet?) -> Void)
+public typealias TextObservationsHandler = ((RecognizeTextConfiguration, @escaping TextObservationSetHandler) throws -> ())
 
 extension UIImage {
 
-    public func recognizedTextSet(for config: RecognizeTextConfiguration, inContentSize contentSize: CGSize) async throws -> RecognizedTextSet {
-        var textSets: [RecognizedTextSet] = []
-        try recognizeTextObservations(configs: [config]) { observationSet in
-            guard let observationSet else {
-                return
-            }
-            let textSet = RecognizedTextSet(
-                config: observationSet.config,
-                texts: self.recognizedTexts(from: observationSet.observations, inContentSize: contentSize)
-            )
-            textSets.append(textSet)
-        }
-        return textSets.first ?? RecognizedTextSet(config: config, texts: [])
+    public func recognizedTextSet(for config: RecognizeTextConfiguration) async throws -> RecognizedTextSet {
+        try await config.recognizedTextSet(recognizeTextObservationsHandler: recognizeTextObservations)
     }
 
-    public func recognizeTextObservations(configs: [RecognizeTextConfiguration], completion: @escaping ((RecognizedTextObservationSet?) -> Void)) throws {
+    public func recognizeTextObservations(
+        config: RecognizeTextConfiguration,
+        completion: @escaping TextObservationSetHandler
+    ) throws {
         guard let cgImage = fixOrientationIfNeeded().cgImage else {
             completion(nil)
             return
         }
-
+        
         let requestHandler = VNImageRequestHandler(cgImage: cgImage)
-        
-        var requests: [VNRecognizeTextRequest] = []
-        for config in configs {
-            let request = VNRecognizeTextRequest { request, error in
-                guard let observations = request.results as? [VNRecognizedTextObservation] else {
-                    return
-                }
-                
-                completion(RecognizedTextObservationSet(config: config, observations: observations))
-            }
-
-            if let languages = config.languages {
-                request.recognitionLanguages = languages
-            }
-            request.recognitionLevel = config.level
-            request.usesLanguageCorrection = config.languageCorrection
-            request.customWords = config.customWords
-            requests.append(request)
-        }
-        
-        try requestHandler.perform(requests)
-    }
-    
-    public func recognizedTexts(from observations: [VNRecognizedTextObservation], inContentSize contentSize: CGSize) -> [RecognizedText] {
-        observations.map { observation in
-            recognizedText(from: observation, inContentSize: contentSize)
-        }
-    }
-
-    public func recognizedText(from observation: VNRecognizedTextObservation, inContentSize contentSize: CGSize) -> RecognizedText
-    {
-        return RecognizedText(observation: observation,
-                              rect: observation.boundingBox,
-                              boundingBox: observation.boundingBox)
-
-//        let width: CGFloat, height: CGFloat
-//        if size.widthToHeightRatio > contentSize.widthToHeightRatio {
-//            width = contentSize.width
-//            height = size.height * width / size.width
-//        } else {
-//            height = contentSize.height
-//            width = size.width * height / size.height
-//        }
-//        let rect = observation.boundingBox.rectForSize(CGSize(width: width, height: height))
-//        return RecognizedText(observation: observation, rect: rect, boundingBox: observation.boundingBox)
+        let request = config.recognizeTextRequest(completion: completion)
+        try requestHandler.perform([request])
     }
 }
 
+extension RecognizedTextObservationSet {
+    
+    var textSet: RecognizedTextSet {
+        let texts = observations.map {
+            RecognizedText(observation: $0, rect: $0.boundingBox, boundingBox: $0.boundingBox)
+        }
+        let textSet = RecognizedTextSet(config: config, texts: texts)
+        return textSet
+    }
+}
