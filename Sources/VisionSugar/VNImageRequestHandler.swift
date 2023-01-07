bef13547b99ca34c4767f5d2e76b43dc062d05a6
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-07 14:08:05 +0500

    tag increment

---
+++ Sources/VisionSugar/VNImageRequestHandler.swift
@@ -0,0 +1,66 @@
+import UIKit
+import Vision
+import SwiftUISugar
+
+extension VNImageRequestHandler {
+    public func recognizedTextSet(
+        for config: RecognizeTextConfiguration = .accurate,
+        includeBarcodes: Bool = false
+    ) async throws -> RecognizedTextSet {
+        if includeBarcodes {
+            return try await config.recognizedTextSet(textAndBarcodesHandler: recognizeTextAndBarcodeObservations)
+        } else {
+            return try await config.recognizedTextSet(textHandler: recognizeTextObservations)
+        }
+    }
+
+    public func recognizedBarcodes() async throws -> [RecognizedBarcode] {
+        var barcodes: [RecognizedBarcode] = []
+        try recognizeBarcodeObservations {
+            barcodes = $0
+        }
+        return barcodes
+    }
+
+    //MARK: - Private
+
+    private func recognizeBarcodeObservations(
+        completion: @escaping BarcodesHandler
+    ) throws {
+        var barcodes: [RecognizedBarcode] = []
+        let barcodesRequest = VNDetectBarcodesRequest.request { _barcodes in
+            barcodes = _barcodes
+        }
+        try perform([barcodesRequest])
+        completion(barcodes)
+    }
+    
+    private func recognizeTextAndBarcodeObservations(
+        config: RecognizeTextConfiguration,
+        completion: @escaping TextAndBarcodesHandler
+    ) throws {
+        var textObservationSet: RecognizedTextObservationSet? = nil
+        var barcodes: [RecognizedBarcode] = []
+        
+        let textRequest = config.recognizeTextRequest { _textObservationSet in
+            textObservationSet = _textObservationSet
+        }
+        
+        let barcodesRequest = VNDetectBarcodesRequest.request { _barcodes in
+            barcodes = _barcodes
+        }
+        
+        try perform([textRequest, barcodesRequest])
+        
+        completion(textObservationSet, barcodes)
+    }
+
+    private func recognizeTextObservations(
+        config: RecognizeTextConfiguration,
+        completion: @escaping TextObservationSetHandler
+    ) throws {
+        let request = config.recognizeTextRequest(completion: completion)
+        try self.perform([request])
+    }
+    
+}
