603c843e2482d44233259a4eb38012114c859054
Ahmed Khalaf <pxlshpr@gmail.com>
2023-06-29 10:34:34 +0500

    tag increment

--- Sources/VisionSugar/Structs/Array+RecognizedText.swift
+++ Sources/VisionSugar/Structs/Array+RecognizedText.swift
@@ -5,3 +5,12 @@ public extension Array where Element == RecognizedText {
         map{$0.boundingBox}.union
     }
 }
+
+public extension Array where Element == CGRect {
+    var union: CGRect {
+        guard !isEmpty else { return .zero }
+        return reduce(.null) { partialResult, rect in
+            partialResult.union(rect)
+        }
+    }
+}
